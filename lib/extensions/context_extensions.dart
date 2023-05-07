fb139b1acfe1aa07552b82793aabd34e3de80c4d
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2023-05-07 15:16:14 +0700

    [+] init commit

---
+++ lib/extensions/context_extensions.dart
@@ -0,0 +1,15 @@
+import 'package:faltool/faltool.dart';
+import 'package:flutter/widgets.dart';
+
+extension ContextExtension on BuildContext {
+  Size get screenSize => MediaQuery.of(this).size;
+
+  double get screenHeight => screenSize.height;
+
+  double get screenHeightWithoutInsets =>
+      screenSize.height -
+      MediaQuery.of(this).viewInsets.top -
+      MediaQuery.of(this).viewInsets.bottom;
+
+  double get screenWidth => screenSize.width;
+}
